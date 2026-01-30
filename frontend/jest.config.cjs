ac5ac9599c9c923902004dff6f634555e86f4461
lanechase34 <lanechase34@outlook.com>
2026-01-30 08:36:17 -0500

    Frontend tests github actions

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -10,4 +10,17 @@ module.exports = {
         '^@test-utils$': '<rootDir>/src/test-utils.tsx',
     },
     setupFilesAfterEnv: [],
+    // Coverage configuration
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.type.ts',
+        '!src/**/__tests__/**',
+        '!src/**/*.test.{ts,tsx}',
+        '!src/**/*.spec.{ts,tsx}',
+        '!src/index.tsx',
+        '!src/App.tsx',
+        '!src/test-utils.tsx',
+    ],
+    verbose: true,
+    maxWorkers: process.env.CI ? 2 : '50%', // max workers for CI
 };
